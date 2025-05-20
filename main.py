e8921089b297dfe853482873fe5363bb067fcce7
Zaberuskina, Anna <azaberuskina@prosv.ru>
2025-05-21 00:23:28 +0300

    added!!!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selene import browser, be, have, by
 
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('html').should(have.text('Мы зарегистрировали подозрительный трафик!!!!'))
+browser.element('html').should(have.text('Мы зарегистрировали подозрительный трафик!!!!!!'))
 
 
 # browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
